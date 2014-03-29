a076717b4d0ddf6498cbcd187b215931f9c8fccf
Gliath <lukebuitendijk@gmail.com>
2014-03-29 20:29:16 +0100

    in de header & signup controller wordt op de response een user toegekend aan Global.user & window.user. Ook wordt Global.authenticated op true gezet. Dit fixt de niet 'verversen' van de pagina. (de pagina update nu single paged)

--- public/js/controllers/header.js
+++ public/js/controllers/header.js
@@ -11,14 +11,18 @@ angular.module('chat.system').controller('HeaderController', ['$scope', 'Global'
     $scope.submit = function() {
     	if($scope.username && $scope.password) {
     		SignInAuth.login({username: $scope.username, password: $scope.password}, function(user) {
-                window.user = { __v: user.__v,
+
+                var tmpUser = { __v: user.__v,
                                 _id: user._id,
                                 email: user.email,
                                 name: user.name,
                                 provider: user.provider,
                                 username: user.username};
+
+                window.user = tmpUser;
+                Global.user = tmpUser;
+                Global.authenticated = true;
             });
-            $scope.$apply();
     	}
     };
 
--- public/js/controllers/signup.js
+++ public/js/controllers/signup.js
@@ -7,7 +7,17 @@ angular.module('chat.system').controller('SignUpController', ['$scope', 'Global'
     $scope.submit = function() { 
     	if($scope.fullname && $scope.email && $scope.username && $scope.password) {
     		SignUpAuth.create({name: $scope.fullname, email: $scope.email, username: $scope.username, password: $scope.password}, function(user) {
-                window.user = user;
+
+                var tmpUser = { __v: user.__v,
+                                _id: user._id,
+                                email: user.email,
+                                name: user.name,
+                                provider: user.provider,
+                                username: user.username};
+
+                window.user = tmpUser;
+                Global.user = tmpUser;
+                Global.authenticated = true;
             });
     	}
     };
